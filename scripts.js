6ecead92b48b1e70dd525b75919a7bc6697e023d
marcosz-96 <marcosz-96@users.noreply.github.com>
2025-04-27 15:29:42 -0300

    Se corrigieron ciertas funcionalidades del programa

--- scripts.js
+++ scripts.js
@@ -1,110 +1,111 @@
 const url = "https://api.restful-api.dev/objects";
 
 window.onload = function(){
-    $('#popUp').hide()
-    getObjects()
+    $('#popUp').hide();
+    getObjects();
 }
 
 // Creamos las PROMESAS que utilizaremos 
-// Verificamos el correcto funcionamiento de la red
+// Función para cargar datos desde la API
 
 function loadObjects(){
     return new Promise(function(resolve, reject){
-        const request = new XMLHttpRequest()
-        request.open("GET", url)
-        request.responseType = "json"
+        const request = new XMLHttpRequest();
+        request.open("GET", url);
+        request.responseType = "json";
         request.onload = function(){
             if(request.status === 200){
-                resolve(request.response)
+                resolve(request.response);
             }else{
-                reject(Error(request.statusText))
+                reject(Error(request.statusText));
             }
-        }
+        };
         request.onerror = function(){
-            reject(Error("Error: unexpected network error."))
-        }
-        request.send()
-    })
+            reject(Error("Error: unexpected network error."));
+        };
+        request.send();
+    });
 }
 
 // Función para agregar Objetos a la lista
 
 function addObject(){
     return new Promise(function(resolve, reject){
-        const request = new XMLHttpRequest()
-        request.open("POST", url)
-        request.setRequestHeader("Content-Type", "application/json")
+        const request = new XMLHttpRequest();
+        request.open("POST", url);
+        request.setRequestHeader("Content-Type", "application/json");
         const data = JSON.stringify({
             'surname': document.getElementById('surname').value, 
             'age': document.getElementById('age').value,
             'email': document.getElementById('email').value
-        })
+        });
         const object = JSON.stringify({
             'name': document.getElementById('name').value,
             'data': data
-        })
+        });
         request.onload = function(){
             if(request.status === 200){
-                resolve(request.response)
+                resolve(request.response);
             }else{
-                reject(Error(request.statusText))
+                reject(Error(request.statusText));
             }
-        }
+        };
         request.onerror = function(){
-            reject(Error("Error: unexpected network error."))
-        }
-        request.send(object)
-    })
+            reject(Error("Error: unexpected network error."));
+        };
+        request.send(object);
+    });
 }
 
 // Función para eliminar Objetos de las lista, por id
 
-function removeObject(){
+function removeObject(id){
     return new Promise(function(resolve, reject){
-        const request = new XMLHttpRequest()
-        request.open('DELETE', url + `/${id}`)
+        const request = new XMLHttpRequest();
+        request.open('DELETE', url + `/${id}`);
         request.onload = function(){
             if(request.status === 200){
-                resolve(request.response)
+                resolve(request.response);
             }else{
-                reject(Error(request.statusText))
+                reject(Error(request.statusText));
             }
-        }
+        };
         request.onerror = function(){
-            reject(Error("Error: unexpected network error."))
-        } 
-        request.send()
-    })
+            reject(Error("Error: unexpected network error."));
+        };
+        request.send();
+    });
 }
 
 // Función para modificar Objetos de la lista, por id
 
 function modifyObject(){
     return new Promise(function(resolve, reject){
-        const request = new XMLHttpRequest()
-        request.open('PUT', url + `/${document.getElementsByName('id2')[0].value}`)
-        request.setRequestHeader('Content-Type', 'application/json')
+        const request = new XMLHttpRequest();
+        const id = document.getElementById('id2')[0].value;
+        request.open('PUT', url + `/${id}`);
+        request.setRequestHeader('Content-Type', 'application/json');
         const data = JSON.stringify({
             'surname': document.getElementById('surname2').value, 
             'age': document.getElementById('age2').value,
             'email': document.getElementById('email2').value
-        })
+        });
         const object = JSON.stringify({
             'name': document.getElementById('name2').value,
             'data': data
-        })
+        });
         request.onload = function(){
             if(request.status === 200){
-                resolve(request.response)
+                resolve(request.response);
             }else{
-                reject(Error(request.statusText))
+                reject(Error(request.statusText));
             }
-        }
+        };
         request.onerror = function(){
-            reject(Error('Error: unexpected network error.'))
-        }
-        request.send(object)
-    })
+            reject(Error('Error: unexpected network error.'));
+        };
+        request.send(object);
+    });
 }
 
 // Creamos las funciones que consumen las PROMESAS 
@@ -112,19 +113,19 @@ function modifyObject(){
 
 function getObjects(){
     loadObjects().then(response => {
-        const tbody = document.querySelector('tbody')
-        tbody.innerHTML = ''
+        const tbody = document.querySelector('tbody');
+        tbody.innerHTML = '';
         response.forEach(object => {
-            if(object.data !== null && Object.hasOwn(object.data, 'name') && Object.hasOwn(object.data, 'surname') && Object.hasOwn(object.data, 'email')){
+            if(object.data !== null && Object.hasOwn(object.data, 'name') && Object.hasOwn(object.data, 'surname') && Object.hasOwn(object.data, 'age') && Object.hasOwn(object.data, 'email')){
                 insertTr(object, false)
             }  
-        })
+        });
     }).catch(reason =>{
-        console.error(reason)
-    })
+        console.error(reason);
+    });
 }
 
-// Función para guardar un Objetos
+// Función para guardar un Objeto
 
 function saveObjects(){
     if(document.getElementById('name').value.trim() !== '' &&
@@ -132,13 +133,17 @@ function saveObjects(){
         document.getElementById('age').value.trim() !== '' && 
         document.getElementById('email').value.trim() !== ''){
         addObject().then((response) =>{
-            const object = JSON.parse(response)
-            const data = JSON.parse(object.data)
-            object.data = data
-            insertTr(object, true)
+            const object = JSON.parse(response);
+            const data = JSON.parse(object.data);
+            object.data = data;
+            insertTr(object, true);
+            alert("Datos ingresados exitosamente!");
         }).catch(reason =>{
-            console.error(reason)
-        })
+            console.error(reason);
+            alert("Error al tratar de ingresar datos.");
+        });
+    }else{
+        alert("Debe completar los campos.");
     }
 }
 
@@ -146,114 +151,109 @@ function saveObjects(){
 
 function deleteObject(object){
     removeObject(object.id).then(()=>{
-        const rows = document.querySelector('tr')
+        const rows = document.querySelectorAll('tr');
         rows.forEach(row => {
             if(row.getAttribute('id') === object.id){
-                row.remove()
-                clearInputs()
+                row.remove();
+                clearInputs();
             }
-        })
+        });
     }).catch(reason => {
-        console.error(reason)
-    })
+        console.error(reason);
+    });
 }
 
 // Función para Actualizar datos de un Objeto
 
 function updateObject(){
-    if(document.getElementById('name2')[0].value.trim() !== '' &&
-    document.getElementById('surname2')[0].value.trim() !== '' &&
-    document.getElementById('age2')[0].value.trim() !== '' &&
-    document.getElementById('email2')[0].value.trim() !== ''){
+    if(document.getElementById('name2').value.trim() !== '' &&
+    document.getElementById('surname2').value.trim() !== '' &&
+    document.getElementById('age2').value.trim() !== '' &&
+    document.getElementById('email2').value.trim() !== ''){
+        
         modifyObject().then(()=>{
-            const rows = document.querySelectorAll('tr')
+            const rows = document.querySelectorAll('tr');
             rows.forEach(row => {
-                if(row.getAttribute('id') === document.getElementsByName('id2')[0].value){
+                if(row.getAttribute('id') === document.getElementById('id2').value){
                     const data = JSON.stringify({
-                        'surname': document.getElementsByName('surname2')[0].value,
-                        'age': document.getElementsByName('age2')[0].value,
-                        'email': document.getElementsByName('email2')[0].value
-                    })
-                    const object = JSON.stringify({
-                        'id': document.getElementsByName('id2')[0].value,
-                        'name': document.getElementsByName('name2')[0].value,
+                        'surname': document.getElementById('surname2').value,
+                        'age': document.getElementById('age2').value,
+                        'email': document.getElementById('email2').value
+                    });
+                    const objectData = JSON.stringify({
+                        'id': document.getElementById('id2').value,
+                        'name': document.getElementById('name2').value,
                         'data': data
-                    })
-                    row.childNodes[1].innerHTML = document.getElementsByName('name2')[0].value,
-                    row.childNodes[2].innerHTML = document.getElementsByName('surname2')[0].value,
-                    row.childNodes[3].innerHTML = document.getElementsByName('age2')[0].value,
-                    row.childNodes[4].innerHTML = document.getElementsByName('email2')[0].value
+                    });
+                    row.children[1].innerHTML = document.getElementById('name2').value,
+                    row.children[2].innerHTML = document.getElementById('surname2').value,
+                    row.children[3].innerHTML = document.getElementById('age2').value,
+                    row.children[4].innerHTML = document.getElementById('email2').value
                     
-                    row.childNodes[5].innerHTML = `<button onclick='viewObject(${object})'>VIEW</button>`
-                    row.childNodes[6].innerHTML = `<button onclick='updateObject(${object})'>MODIFY</button>`
-                    row.childNodes[7].innerHTML = `<button onclick='deleteObject(${object})'>DELETE</button>`
+                    row.children[5].innerHTML = `<button onclick='viewObject(${objectData})'>VIEW</button>`;
+                    row.children[6].innerHTML = `<button onclick='updateObject(${objectData})'>MODIFY</button>`;
+                    row.children[7].innerHTML = `<button onclick='deleteObject(${objectData})'>DELETE</button>`;
                 }
-            })
-            $('#popUp').dialog('close')
-            clearInputs()
+            });
+            $('#popUp').dialog('close');
+            clearInputs();
         }).catch(reason=>{
-            console.error(reason)
-        })
+            console.error(reason);
+        });
     }
 }
 
 // Funciones de Agregados
 
 function insertTr(object, canChange){
-    const tbody = document.querySelector('tbody')
-    const row = tbody.insertRow()
-    row.setAttribute('id', object.id)
-    const id = row.insertCell()
-    id.innerHTML = object.id
-    const name = row.insertCell()
-    name.innerHTML = object.name
-    const surname = row.insertCell()
-    surname.innerHTML = object.data.surname
-    const age = row.insertCell()
-    age.innerHTML = object.data.age
-    const email = row.insertCell()
-    email.innerHTML = object.data.email
+    const tbody = document.querySelector('tbody');
+    const row = tbody.insertRow();
+    row.setAttribute('id', object.id);
+
+    row.insertCell().innerHTML = object.id;
+    row.insertCell().innerHTML = object.name;
+    row.insertCell().innerHTML = object.data.surname;
+    row.insertCell().innerHTML = object.data.age;
+    row.insertCell().innerHTML = object.data.email;
+
     const data = JSON.stringify({
         'surname': object.data.surname,
         'age': object.data.age,
         'email': object.data.email
-    })
-    var object = JSON.stringify({
+    });
+    const objectData = JSON.stringify({
         'id': object.id,
         'name': object.name,
         'data': data
-    })
+    });
     if (canChange) {
-        const view = row.insertCell()
-        view.innerHTML = `<button onclick='viewObject(${object})'>VIEW</button>`
-        const modify = row.insertCell()
-        modify.innerHTML = `<button onclick='modifyObject(${object})'>MODIFY</button>`
-        const delet = row.insertCell()
-        delet.innerHTML = `<button onclick='deleteObject(${object})'>DELETE</button>`
+        row.insertCell().innerHTML = `<button onclick='viewObject(${objectData})'>VIEW</button>`;
+        row.insertCell().innerHTML = `<button onclick='modifyObject(${objectData})'>MODIFY</button>`;
+        row.insertCell().innerHTML = `<button onclick='deleteObject(${objectData})'>DELETE</button>`;
     }
-    clearInputs()
+    clearInputs();
 }
 
 // Función para Ver los Objetos
 
 function viewObject(object){
     const data = JSON.parse(object.data)
-    document.getElementsByName('id2')[0].value = object.id
-    document.getElementsByName('name2')[0].value = object.name
-    document.getElementsByName('surname2')[0].value = data.surname
-    document.getElementsByName('age2')[0].value = data.age
-    document.getElementsByName('email2')[0].value = data.email
+    document.getElementById('id2').value = object.id;
+    document.getElementById('name2').value = object.name;
+    document.getElementById('surname2').value = data.surname;
+    document.getElementById('age2').value = data.age;
+    document.getElementById('email2').value = data.email;
     $('#popUp').dialog({
         closeText: ''
-    }).css('font-size', '15px')
+    }).css('font-size', '15px');
 }
 
 // Funcion para Vaciar los inputs despues de cargar datos
 
 function clearInputs(){
-    document.getElementById('name').value = ''
-    document.getElementById('surname').value = ''
-    document.getElementById('age').value = ''
-    document.getElementById('email').value = ''
-    document.getElementById('name').focus()
+    document.getElementById('name').value = '';
+    document.getElementById('surname').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('name').focus();
 }
